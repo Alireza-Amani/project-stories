f404ed39b3bfb525fda14da152f131b311a987e6
Alireza Amani <alireza.amani101@gmail.com>
2026-03-22 13:42:20 +0100

    correct vite.config for multipage website

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+// Recreate __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: "/project-stories/",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        flashDenken: resolve(__dirname, "flash-denken.html"), // Add your new page here
+      },
+    },
   },
 });
